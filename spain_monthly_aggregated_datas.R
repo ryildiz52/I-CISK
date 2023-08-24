057b67e620b954c7fc93e1dd790f2273b6f93889
ryildiz52 <129756176+ryildiz52@users.noreply.github.com>
2023-08-24 15:48:52 +0200

    monthly_aggregated

---
+++ spain_monthly_aggregated_datas.R
@@ -0,0 +1,41 @@
+library(sf)
+library(dplyr)
+library(lubridate)
+library(stringr)
+
+geopackage_path <- "C:/Users/yagmur/work/Spain/filtered_out-data/filtered_datas"
+
+# Get a list of all the files in the directory
+geopackage_files <- list.files(geopackage_path, pattern = "^filtered_.*\\.gpkg$", full.names = TRUE)
+
+# Loop through each file
+for (filepath in geopackage_files) {
+  # Extract the year from the filename
+  year <- str_extract(filepath, "\\d{4}")
+  # Read the GeoPackage file
+  data <- st_read(filepath)
+  # Convert the timestamp column to a date
+  data$timestamp <- as.Date(data$timestamp)
+  # Extract year and month from the timestamp
+  data$year <- year(data$timestamp)
+  data$month <- month(data$timestamp)
+  # Group by station, year, and month, and summarize the values
+  monthly_data <- data %>%
+    group_by(station_id, station_name, year, month) %>%
+    summarize(
+      mean_temperature = mean(mean_temperature, na.rm = TRUE),
+      min_temperature = min(min_temperature, na.rm = TRUE),
+      max_temperature = max(max_temperature, na.rm = TRUE),
+      precipitation = mean(precipitation, na.rm = TRUE)
+    ) %>%
+    ungroup()
+  # Filter out rows with no calculations (all variables are NA)
+  monthly_data_filtered <- monthly_data %>%
+    group_by(station_id, station_name, year, month) %>%
+    filter(n() > 0)  # Keep rows with at least one entry for the month
+  # Create the filename for the aggregated data
+  aggregated_filename <- paste0(year, "_agg_months.gpkg")
+  aggregated_filepath <- file.path(geopackage_path, aggregated_filename)
+  # Save the aggregated monthly data as a new GeoPackage file
+  st_write(monthly_data_filtered, aggregated_filepath)
+}  
